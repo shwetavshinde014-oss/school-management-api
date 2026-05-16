3150c044e1a6213f44b43f7f8ade3d64c705c21f
shwetavshinde014-oss <shwetavshinde014@gmail.com>
2026-05-16 12:59:37 +0530

    Updated error handling

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed");
+    console.log(err);
   } else {
     console.log("MySQL Connected");
   }
